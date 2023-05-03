e1ebc62c87537f24d16532d82faecc39f3096a26
Somov Alexey Alexandrovich <a.somov@robo.finance>
2023-05-03 20:44:53 +0700

    Bad Completed 12-kubernetes-03 part 3

--- 12-kubernetes-03/src/init.sh
+++ 12-kubernetes-03/src/init.sh
@@ -1,6 +1,23 @@
-curl -s nginx-svc:80; if test "$?" = "6"; then exit 1; else exit 0; fi
+#curl -s nginx-svc:80; if test "$?" = "6"; then exit 1; else exit 0; fi
 # until sh init.sh; do echo waiting for nginx-svc; sleep 3; done
 # until (curl -s nginx-svc:80; if test '$?' = '6'; then exit 1; else exit 0; fi); do echo waiting for nginx-svc; sleep 3; done
 # until (curl -s http://example.com; if test '$?' = '6'; then exit 1; else exit 0; fi); do echo waiting for nginx-svc; sleep 3; done
 # until [curl -s http://example.com; if test '$?' = '6'; then exit 1; else exit 0; fi]; do echo waiting for nginx-svc; sleep 3; done
 #curl -s http://example.com; if test "$?" = "6"; then exit 1; else exit 0; fi
+
+while true
+do
+   curl -s nginx-svc:80;
+   res=$?
+
+   if test "$res" = "7"; then
+     exit 0;
+   fi
+
+   if test "$res" = "0"; then
+     exit 0;
+   fi
+
+   echo "waiting for nginx-svc"
+   sleep 3
+done
